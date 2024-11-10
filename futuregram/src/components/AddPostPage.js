44d9707889e373073e780f80db8f1ca5fa5e6140
arsh <arshbansal2020@gmail.com>
2024-11-10 03:23:05 -0600

    Restyled

--- futuregram/src/components/AddPostPage.js
+++ futuregram/src/components/AddPostPage.js
@@ -4,10 +4,48 @@ import { db } from '../firebaseConfig';
 import { collection, addDoc } from 'firebase/firestore';
 import { useNavigate } from 'react-router-dom';
 
+/*
+import DatePicker from 'react-datepicker';
+import TimePicker from 'react-time-picker';
+
+import 'react-datepicker/dist/react-datepicker.css';
+*/
+
+
 function AddPostPage() {
+  const backgroundStyle = {
+    backgroundImage: 'url("/post.jpg")',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    minHeight: '100vh',
+    display: 'flex',
+    //alignItems: 'center',
+    justifyContent: 'center',
+  };
+
   const [fileName, setFileName] = useState('');
   const [delay, setDelay] = useState(''); // Delay time in minutes
   const navigate = useNavigate();
+  
+  /*
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedTime, setSelectedTime] = useState(new Date());
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+  
+  const handleTimeChange = (time) => {
+    setSelectedTime(time);
+  };
+
+  const combinedDateTime = new Date(selectedDate);
+  combinedDateTime.setHours(selectedTime.getHours());
+  combinedDateTime.setMinutes(selectedTime.getMinutes());
+  combinedDateTime.setSeconds(selectedTime.getSeconds());
+
+  */
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
@@ -46,23 +84,44 @@ function AddPostPage() {
   };
 
   return (
-    <div className="add-post-page">
-      <h2>Add New Post</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="file"
-          onChange={handleFileChange}
-        />
-        <input
-          type="number"
-          placeholder="Time before it's visible (in minutes)"
-          value={delay}
-          onChange={handleDelayChange}
-          min="0"
-          required
-        />
-        <button type="submit" disabled={!fileName || !delay}>Submit</button>
-      </form>
+    <div style={backgroundStyle}>
+      <div className="add-post-page">
+        
+        <h2>Create a <span className="app-title">FutureGram</span> Time Capsule!</h2>
+        <br />
+
+        <form onSubmit={handleSubmit}>
+          <input
+            type="file"
+            onChange={handleFileChange}
+          />
+        
+          <br />
+          <br />
+
+          <input
+            type="number"
+            style={{width: "42%"}}
+            placeholder="Minutes until capsule opens"
+            value={delay}
+            onChange={handleDelayChange}
+            min="0"
+            required
+          />
+        
+          {/*
+          <br />
+          <br />
+          <p>When would you like your FutureGram to open?</p>
+          
+          <DatePicker selected={selectedDate} onChange={handleDateChange} />
+          <TimePicker value={selectedTime} onChange={handleTimeChange} />
+          <p>Selected date: {combinedDateTime.toString()}</p>
+          */}
+          
+          <button type="submit" style={{width: "10%"}} disabled={!fileName || !delay}>Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
